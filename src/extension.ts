df68e360c6a8c70b44b1a98e2b875cad66d4baea
박지원 <jackgg12322@gmail.com>
2024-07-30 16:13:08 +0900

    "snippets.json Refactor"

--- src/extension.ts
+++ src/extension.ts
@@ -2,8 +2,9 @@ import * as vscode from 'vscode';
 import * as path from 'path';
 
 export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand(
-    'react-snippets.insertComponent',
+  // React Arrow Function Component 스니펫 삽입
+  let arrowFunctionDisposable = vscode.commands.registerCommand(
+    'react-snippets.insertArrowFunction',
     async () => {
       const editor = vscode.window.activeTextEditor;
       if (editor) {
@@ -14,21 +15,49 @@ export function activate(context: vscode.ExtensionContext) {
 
         const snippet = new vscode.SnippetString(
           `const ${componentName} = () => {\n` +
-            `\treturn (\n` +
-            `\t\t<>\n` +
-            `\t\t\t$0\n` +
-            `\t\t</>\n` +
-            `\t);\n` +
+            `  return (\n` +
+            `    <>\n` +
+            `      $0\n` +
+            `    </>\n` +
+            `  );\n` +
             `};\n\n` +
             `export default ${componentName};`
         );
 
-        editor.insertSnippet(snippet);
+        await editor.insertSnippet(snippet);
       }
     }
   );
 
-  context.subscriptions.push(disposable);
+  // React Function Component 스니펫 삽입
+  let functionDisposable = vscode.commands.registerCommand(
+    'react-snippets.insertFunction',
+    async () => {
+      const editor = vscode.window.activeTextEditor;
+      if (editor) {
+        const filePath = editor.document.fileName;
+        const fileName = path.basename(filePath, path.extname(filePath));
+        const componentName =
+          fileName.charAt(0).toUpperCase() + fileName.slice(1);
+
+        const snippet = new vscode.SnippetString(
+          `function ${componentName}() {\n` +
+            `  return (\n` +
+            `    <>\n` +
+            `      $0\n` +
+            `    </>\n` +
+            `  );\n` +
+            `}\n\n` +
+            `export default ${componentName};`
+        );
+
+        await editor.insertSnippet(snippet);
+      }
+    }
+  );
+
+  context.subscriptions.push(arrowFunctionDisposable);
+  context.subscriptions.push(functionDisposable);
 }
 
 export function deactivate() {}
